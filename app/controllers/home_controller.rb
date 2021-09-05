8fe9327eb76d0f10389c903031ebbc02730c8ca8
Alice Scholze <alicescholze@outlook.com>
2021-09-05 17:17:34 -0300

    change endpoint to log param id

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
-    render json: 'batatinha'
+    render json: permited_params[:id]
+  end
+
+  private
+
+  def permited_params
+    params.permit(:id)
   end
 end
